5a15e236a6b2ad70da04e91dc69818624081d550
Codeway <codeway0841@gmail.com>
2020-09-14 03:09:26 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import './App.scss';
+import {Header} from './Header/Header'
+import { RentalList } from './Rental/RentalList';
+import { RentalDetail } from './Rental/RentalDetail';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+      <div className="container-fluid">
+        <Route exact path="/" render={()=> <Redirect to='/rentals'/> }/>
+        <Route exact path="/rentals" component={RentalList}/>
+        <Route exact path="/rentals/:id" component={RentalDetail}/>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
